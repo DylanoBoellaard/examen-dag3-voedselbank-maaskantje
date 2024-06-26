75f7abe172bccf0648dccfb90ac08d11744be75d
DylanoBoellaard <336751@student.mboutrecht.nl>
2024-06-26 11:20:06 +0200

    Added nullable to certain records

--- database/migrations/2024_06_26_091126_persoon.php
+++ database/migrations/2024_06_26_091126_persoon.php
@@ -18,9 +18,10 @@ return new class extends Migration
             $table->foreignId('gezinId') // Declare foreign key
                 ->references('id')
                 ->on('gezin')
-                ->onDelete('cascade'); // Cascade = if record in referenced table gets deleted, all related records in the current table will also be deleted
+                ->onDelete('cascade') // Cascade = if record in referenced table gets deleted, all related records in the current table will also be deleted
+                ->nullable();
             $table->string('voornaam', 100);
-            $table->string('tussenvoegsel', 50);
+            $table->string('tussenvoegsel', 50)->nullable();
             $table->string('achternaam', 100);
             $table->date('geboortedatum');
             $table->string('typePersoon', 50);
