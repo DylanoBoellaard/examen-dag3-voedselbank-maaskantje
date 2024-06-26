7b910dd266a065966d701b7c40b92a1042460009
SimonPras <332959@student.mboutrecht.nl>
2024-06-26 11:12:42 +0200

    contact and contactpergezin seeders filled
    I filled the contact and contactpergezin seeders with data

--- database/seeders/ContactPerGezin.php
+++ database/seeders/ContactPerGezin.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ContactPerGezin extends Seeder
 {
@@ -12,6 +13,15 @@ class ContactPerGezin extends Seeder
      */
     public function run(): void
     {
-        //
+        $contactPerGezin = [
+            ['gezinId' => 1, 'contactId' => 1],
+            ['gezinId' => 2, 'contactId' => 2],
+            ['gezinId' => 3, 'contactId' => 3],
+            ['gezinId' => 4, 'contactId' => 4],
+            ['gezinId' => 5, 'contactId' => 5],
+            ['gezinId' => 6, 'contactId' => 6],
+        ];
+
+        DB::table('contactPerGezin')->insert($contactPerGezin);
     }
 }
