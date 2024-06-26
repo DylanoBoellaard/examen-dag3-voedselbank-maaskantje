1250b347de86fa8c6d001ba62a1cf3e0a7a2d72c
vincent van de merwe <335479@student.mboutrecht.nl>
2024-06-26 15:55:00 +0200

    i added a table and a title to the overzicht page also i added a forelse with an @empty so it will loop through the $data if it has data otherwise it show 1 row with a message that there aren't products with that categorie and in the web.php i added a second route to the overzicht page but the 2e one is for when the user submits the form also i added comments to the new code in these files and i added comments to the existing code in these files

--- resources/views/voorraad/overzicht.blade.php
+++ resources/views/voorraad/overzicht.blade.php
@@ -1,1 +1,70 @@
+<!-- gets the template from the layouts folder -->
 @include('./layouts/app')
+
+<div>
+    <!-- title of the overzicht page -->
+    <h2>Overzicht ProductVoorraden</h2>
+    <!-- form for the user to submit when they choose a categorie -->
+    <form action="{{route('overzicht')}}" method="post">
+        <!-- protection key -->
+        @csrf
+        <!-- laravel form method to submit -->
+        @method('post')
+        <!-- select with the name categorie and alot of options to choose from -->
+        <select name="categorie">
+            <option value="">Selecteer Categorie</option>
+            <option value="AGF">AGF</option>
+            <option value="KV">KV</option>
+            <option value="ZPE">ZPE</option>
+            <option value="BB">BB</option>
+            <option value="FSKT">FSKT</option>
+            <option value="PRW">PRW</option>
+            <option value="SSKO">SSKO</option>
+            <option value="SKCC">SKCC</option>
+            <option value="BVH">BVH</option>
+        </select>
+        <!-- submits the form -->
+        <input type="submit" value="Toon Voorraad">
+    </form>
+</div>
+
+<table>
+    <thead>
+        <!-- table heads for the table columns -->
+        <tr>
+            <th>Productnaam</th>
+            <th>Categorie</th>
+            <th>Eenheid</th>
+            <th>Aantal</th>
+            <th>Houdbaarheidsdatum</th>
+            <th>Magazijn</th>
+            <th>Voorraad Details</th>
+        </tr>
+    </thead>
+    <tbody>
+        <!-- if $data has data then show go through the data and make the rows with the data -->
+        @forelse ($data as $voorraad)
+        <tr>
+            <td>{{$voorraad->Product_naam}}</td>
+            <td>{{$voorraad->Categorie_naam}}</td>
+            <td>{{$voorraad->VerpakkingsEenheid}}</td>
+            <td>{{$voorraad->Aantal}}</td>
+            <td>{{$voorraad->Houdbaarheidsdatum}}</td>
+            <td>{{$voorraad->MagazijnId}}</td>
+            <td>
+                <a href="">
+                    <img class="small-img" src="/img/note.png" alt="note.png">
+                </a>
+            </td>
+        </tr>
+        <!-- if $data is empty then show this message under the table -->
+        @empty
+        <tr>
+            <td colspan="7">Er zijn geen producten bekent die behoren bij de geselecteerde productcategorie</td>
+        </tr>
+        @endforelse
+    </tbody>
+</table>
+
+<!-- link back to the homepage -->
+<a href="{{route('homepage')}}">home</a>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [VoorraadController::class, 'homepage']);
+// when you go to the website then you land on this route and the homepage
+Route::get('/', [VoorraadController::class, 'homepage'])->name('homepage');
 
+// when the user clicks on the overzicht page link then they get send to the overzicht page through this route
 Route::get('/overzicht', [VoorraadController::class, 'Overzicht_Voorraad'])->name('overzicht');
+
+// when the user submits a form then they get send to the overzicht page with the data with the user inputs in the query
+Route::post('/overzicht', [VoorraadController::class, 'Overzicht_Voorraad'])->name('overzicht');
