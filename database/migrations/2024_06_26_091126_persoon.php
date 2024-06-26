83d0e271a63b5d7a1bc7273e02a08999bc3b0817
DylanoBoellaard <336751@student.mboutrecht.nl>
2024-06-26 11:16:38 +0200

    Added persoon table

---
+++ database/migrations/2024_06_26_091126_persoon.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Create table persoon
+        Schema::dropIfExists('persoon');
+        Schema::create('persoon', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('gezinId') // Declare foreign key
+                ->references('id')
+                ->on('gezin')
+                ->onDelete('cascade'); // Cascade = if record in referenced table gets deleted, all related records in the current table will also be deleted
+            $table->string('voornaam', 100);
+            $table->string('tussenvoegsel', 50);
+            $table->string('achternaam', 100);
+            $table->date('geboortedatum');
+            $table->string('typePersoon', 50);
+            $table->boolean('isVertegenwoordiger')->default(0);
+            $table->boolean('isActief')->default(true);
+            $table->string('opmerkingen', 250)->nullable();
+            $table->timestamps(6);
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('persoon');
+    }
+};
