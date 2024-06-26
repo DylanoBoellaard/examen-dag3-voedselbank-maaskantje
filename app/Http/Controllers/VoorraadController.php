e8cedd48dfffdf7be813970a22a59cc46762fd7e
vincent van de merwe <335479@student.mboutrecht.nl>
2024-06-26 15:51:02 +0200

    added functionality to the overzicht page with the data in the table and that the user can submit a form with user input so the user can search on the categorie also i added comments to all the code in the controller

--- app/Http/Controllers/VoorraadController.php
+++ app/Http/Controllers/VoorraadController.php
@@ -2,17 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProductPerMagazijn;
 use Illuminate\Http\Request;
 
 class VoorraadController extends Controller
 {
+    // this methods send the user to the homepage
     public function homepage()
     {
         return view('homepage');
     }
 
-    public function Overzicht_Voorraad()
+    // this function sends the user to the overzicht voorraad page
+    // when the user fill the form and submits the form the this method will catch it
+    // and shows the data based on the users input
+    public function Overzicht_Voorraad(Request $request)
     {
-        return view('voorraad.overzicht');
+
+        // checks if the request is not empty except when there is a categorie in the request
+        // otherwise it goes to the else
+        if (!empty($request->except('categorie'))) {
+            // this gets the data with the user inputs
+            $data = ProductPerMagazijn::select('ProductPerMagazijn.id', 'ProductPerMagazijn.ProductId', 'ProductPerMagazijn.MagazijnId', 'Product.Naam as Product_naam', 'Product.CategorieId', 'Product.Houdbaarheidsdatum', 'Magazijn.VerpakkingsEenheid', 'Magazijn.Aantal', 'Categorie.Naam as Categorie_naam')->join('Magazijn', 'ProductPerMagazijn.MagazijnId', 'Magazijn.Id')->join('Product', 'ProductPerMagazijn.ProductId', 'Product.Id')->join('Categorie', 'Product.CategorieId', 'Categorie.Id')->where('Categorie.Naam', $request->categorie)->get();
+
+            // this sends the user to the overzicht page with data
+            return view('voorraad.overzicht', compact('data'));
+        } else {
+            // just only gets the data from the database without user input
+            $data = ProductPerMagazijn::select('ProductPerMagazijn.id', 'ProductPerMagazijn.ProductId', 'ProductPerMagazijn.MagazijnId', 'Product.Naam as Product_naam', 'Product.CategorieId', 'Product.Houdbaarheidsdatum', 'Magazijn.VerpakkingsEenheid', 'Magazijn.Aantal', 'Categorie.Naam as Categorie_naam')->join('Magazijn', 'ProductPerMagazijn.MagazijnId', 'Magazijn.Id')->join('Product', 'ProductPerMagazijn.ProductId', 'Product.Id')->join('Categorie', 'Product.CategorieId', 'Categorie.Id')->get();
+
+            // sends the user with the data to the overzicht page
+            return view('voorraad.overzicht', compact('data'));
+        }
     }
 }
