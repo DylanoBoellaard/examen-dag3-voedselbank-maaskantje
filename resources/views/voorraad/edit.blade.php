b26fba9908b46c14a57015a8a17c22234bfb2107
vincent van de merwe <335479@student.mboutrecht.nl>
2024-06-27 16:46:18 +0200

    added alot of styling for the edit and details page but also for the messages for errors and successes and for alot of elements also added alot of new elements and alot of classes to the page

--- resources/views/voorraad/edit.blade.php
+++ resources/views/voorraad/edit.blade.php
@@ -1,70 +1,75 @@
 @include('./layouts/app')
 
-<h2>Wijzig Product Details {{$data[0]->Naam}}</h2>
 
-@if(session()->has('succes'))
-<h3 class="succes-text">
-    {{session('succes')}}
-</h3>
-<script>
-    setTimeout(function() {
-        window.location.href = "{{route('product_details', [$data[0]->id])}}";
-    }, 3000);
-</script>
-@endif
+<section class="container">
+    <section class="title">
+        <h2>Wijzig Product Details {{$data[0]->Naam}}</h2>
+    </section>
+</section>
 
-@if(session()->has('error'))
-<h3 class="succes-text">
-    {{session('error')}}
-</h3>
-@endif
+<section class="succes-text">
+    @if(session()->has('succes'))
+    <h3>
+        {{session('succes')}}
+    </h3>
+    <script>
+        setTimeout(function() {
+            window.location.href = "{{route('product_details', [$data[0]->id])}}";
+        }, 3000);
+    </script>
+    @endif
+</section>
 
-<form action="{{route('update', [$data[0]->id])}}" method="post">
+<section class="error-text">
+    @if(session()->has('error'))
+    <h3>
+        {{session('error')}}
+    </h3>
+    @endif
+</section>
+
+<form action="{{route('update', [$data[0]->id])}}" method="post" class="edit-form">
     @csrf
     @method('put')
     <div>
         <label for="Naam">Productnaam</label>
-        <input type="text" name="Naam" value="{{$data[0]->Naam}}">
-    </div>
-    <div>
         <label for="Houdbaarheidsdatum">Houdbaarheidsdatum</label>
-        <input type="text" name="Houdbaarheidsdatum" value="{{date('d-m-Y', strtotime($data[0]->Houdbaarheidsdatum))}}">
-    </div>
-
-    <div>
         <label for="Barcode">Barcode</label>
-        <input type="text" name="Barcode" value="{{$data[0]->Barcode}}">
+        <label for="Locatie">Magazijn locatie</label>
+        <label for="Ontvangstdatum">Ontvangstdatum</label>
+        <label for="AantalUitgeleleverdeProducten">Aantal uitgeleverde producten</label>
+        @if (session()->has('error_message'))
+        <label class="active" for="Uitleveringsdatum">Uitleveringsdatum</label>
+        @else
+        <label for="Uitleveringsdatum">Uitleveringsdatum</label>
+        @endif
+        <label for="Aantal">Aantal op voorraad</label>
+        <input class="submit-edit" type="submit" value="Wijzig Product Details">
     </div>
     <div>
-        <label for="Locatie">Magazijn locatie</label>
+        <input type="text" name="Naam" value="{{$data[0]->Naam}}">
+        <input type="text" name="Houdbaarheidsdatum" value="{{date('d-m-Y', strtotime($data[0]->Houdbaarheidsdatum))}}">
+        <input type="text" name="Barcode" value="{{$data[0]->Barcode}}">
         <select name="Locatie">
             <option value="{{$data[0]->Locatie}}">{{$data[0]->Locatie}}</option>
         </select>
-    </div>
-    <div>
-        <label for="Ontvangstdatum">Ontvangstdatum</label>
         <input type="text" name="Ontvangstdatum" value="{{date('d-m-Y', strtotime($data[0]->Ontvangstdatum))}}">
-    </div>
-    <div>
-        <label for="AantalUitgeleleverdeProducten">Aantal uitgeleverde producten</label>
         <input type="text" name="AantalUitgeleleverdeProducten">
         @if(session()->has('error_message'))
-        <p>
-            {{session('error_message')}}
-        </p>
+        <li class="input-error-message">
+            <span>
+                {{session('error_message')}}
+            </span>
+        </li>
         @endif
-    </div>
-    <div>
-        <label for="Uitleveringsdatum">Uitleveringsdatum</label>
         <input type="date" name="Uitleveringsdatum" value="{{date('Y-m-d', strtotime($data[0]->Uitleveringsdatum))}}">
-    </div>
-    <div>
-        <label for="Aantal">Aantal op voorraad</label>
         <input type="text" name="Aantal" value="{{$data[0]->Aantal}}">
-    </div>
-    <input type="submit" value="Wijzig Product Details">
 </form>
 
-<!-- these links go to the details page and the homepage -->
-<a href="{{route('product_details', [$data[0]->id])}}">terug</a>
-<a href="{{route('homepage')}}">home</a>
+
+<section class="links-edit">
+    <!-- these links go to the details page and the homepage -->
+    <a href="{{route('product_details', [$data[0]->id])}}">terug</a>
+    <a href="{{route('homepage')}}">home</a>
+</section>
+</div>
