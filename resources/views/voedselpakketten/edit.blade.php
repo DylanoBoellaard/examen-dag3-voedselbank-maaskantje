7f882068682d833542e60b0555e68aa8e3bf618a
MFMvanderholst <117632371+MFMvanderholst@users.noreply.github.com>
2024-06-27 16:11:31 +0200

    done

--- resources/views/voedselpakketten/edit.blade.php
+++ resources/views/voedselpakketten/edit.blade.php
@@ -10,20 +10,44 @@
 
 <body>
     <main>
-        @if ($errors->any())
-        <ul>
-            @foreach($errors->all() as $error)
-            <li>'{{ $error }}</li>
-            @endforeach
-        </ul>
-        @endif
+
 
         <form action="/gezin/{{ $voedselpakket->id }}" method="post">
             @csrf
             @method('put')
             <h3>Wijzig voedselpakket status</h3>
 
+
+
+            @if($voedselpakket->id == 3)
             <div>
+                <select name="#" id="selector">
+                    <option value="{{ $voedselpakket->status }}">{{ $voedselpakket->status }}</option>
+                    <option value="Uitgereikt">Uitgereikt</option>
+                    <option value="NietUitgereikt">Niet Uitgereikt</option>
+                    <option value="NietMeerGegeven">Niet Meer Gegeven</option>
+                </select>
+            </div>
+
+            @if ($errors->any())
+            <ul>
+                @foreach($errors->all() as $error)
+                <div class="alert alert-error">
+                    <p>Dit gezin is niet meer ingeschreven bij de voedselbank en daarom kan er geen voedselpakket worden uitgereikt</p>
+                </div>
+                @endforeach
+            </ul>
+            @endif
+
+            @else
+            <div>
+                @if ($errors->any())
+                <ul>
+                    @foreach($errors->all() as $error)
+                    <li>'{{ $error }}</li>
+                    @endforeach
+                </ul>
+                @endif
                 <select name="status" id="selector">
                     <option value="{{ $voedselpakket->status }}">{{ $voedselpakket->status }}</option>
                     <option value="Uitgereikt">Uitgereikt</option>
@@ -37,7 +61,7 @@
                 <p>{{ session('status') }}</p>
             </div>
             @endif
-
+            @endif
             <div class="buttons">
                 <button class="grey" type="submit">Wijzig status voedselpakket</button>
             </div>
